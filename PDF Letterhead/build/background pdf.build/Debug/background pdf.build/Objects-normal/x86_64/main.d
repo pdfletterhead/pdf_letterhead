a9066f8f77d30ada3298391e891fb46b96fb0add
Pim Snel <pim@lingewoud.nl>
2012-08-20 23:06:59 +0200

    try to get gitignore working

--- PDF Letterhead/build/background pdf.build/Debug/background pdf.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/pim/Desktop/Bureaublad\ Lapp5/PDF\ Letterhead/background\ pdf/background\ pdf/main.m
--- PDF Letterhead/build/background pdf.build/Debug/background pdf.build/Objects-normal/x86_64/pmpPreviewView.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pim/Desktop/Bureaublad\ Lapp5/PDF\ Letterhead/background\ pdf/background\ pdf/../pmpPreviewView.m \
-  /Users/pim/Desktop/Bureaublad\ Lapp5/PDF\ Letterhead/background\ pdf/background\ pdf/../pmpPreviewView.h
