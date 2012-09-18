daadc2b7447856e9dda4a98dac88b1fe234b92d1
Pim Snel <pim@lingewoud.nl>
2012-09-18 15:37:31 +0200

    - dragme plaatje niet meer tonen #48 - FAQ toegevoegd #52 - alle targets getest #32

---
+++ PDF Letterhead/PDF Letterhead/PLFAQWindowController.h
@@ -0,0 +1,15 @@
+//
+//  PLFAQWindowController.h
+//  PDF Letterhead
+//
+//  Created by Pim Snel on 18-09-12.
+//  Copyright (c) 2012 Pim Snel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PLFAQWindowController : NSWindowController
+
+@property (assign) IBOutlet NSTextView *FAQView;
+
+@end
