a6cb0ded01989c94e9576f752a97a6a02c1aa3da
Richard Vollebregt <richard@lingewoud.com>
2016-01-07 09:47:21 +0100

    inset shadow on containers

--- PDF Letterhead/PDF Letterhead/PLBackgroundDropZoneContainer.m
+++ PDF Letterhead/PDF Letterhead/PLBackgroundDropZoneContainer.m
@@ -16,19 +16,14 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
-        NSShadow *dropShadow = [[NSShadow alloc] init];
-        [dropShadow setShadowColor:[NSColor redColor]];
-        [dropShadow setShadowOffset:NSMakeSize(0, 50.0)];
-        [dropShadow setShadowBlurRadius:50.0];
-        
-      
-        
-        
+     
         self.layer = _layer;   // strangely necessary
         self.wantsLayer = YES;
         self.layer.masksToBounds = YES;
-        self.layer.cornerRadius = 10.0;
+        self.layer.cornerRadius = 5.0;
+        
+        [self.layer setBorderColor:[NSColor colorWithRed:0.765 green:0.788 blue:0.812 alpha:1].CGColor];
+        [self.layer setBorderWidth:1.0];
 }
     
     return self;
@@ -39,6 +34,30 @@
     background = [NSColor colorWithDeviceRed: 229.0/255.0 green: 232.0/255.0 blue: 236.0/255.0 alpha: 1.0];
     [background set];
     NSRectFill([self bounds]);
+    
+    // Describe an inset rect (adjust for pixel border)
+    NSRect whiteRect = NSInsetRect([self bounds], 0, 0);
+    
+    // Create and fill the shown path
+    NSBezierPath * path = [NSBezierPath bezierPathWithRect:whiteRect];
+    [path fill];
+    
+    // Save the graphics state for shadow
+    [NSGraphicsContext saveGraphicsState];
+    
+    // Set the shown path as the clip
+    [path setClip];
+    
+    // Create and stroke the shadow
+    NSShadow * shadow = [[NSShadow alloc] init];
+    [shadow setShadowColor:[NSColor lightGrayColor]];
+    [shadow setShadowBlurRadius:3.0];
+    [shadow set];
+    [path stroke];
+    
+    // Restore the graphics state
+    [NSGraphicsContext restoreGraphicsState];
+    
 }
 
 @end
