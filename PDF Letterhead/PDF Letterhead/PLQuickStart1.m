ddff7945a6a2f3965b642005bd7a68fe1e16e674
Pim Snel <pim@lingewoud.nl>
2016-01-18 12:59:42 +0100

    upgrade splash screen verbeterd

--- PDF Letterhead/PDF Letterhead/PLQuickStart1.m
+++ PDF Letterhead/PDF Letterhead/PLQuickStart1.m
@@ -17,23 +17,17 @@
 - (id)initWithWindow:(NSWindow *)window
 {
     self = [super initWithWindow:window];
-    if (self) {
-        // Initialization code here.
-        [[self window] setBackgroundColor:[NSColor whiteColor]];
-    }
-    
     return self;
 }
 
 - (void)windowDidLoad
 {
-    [super windowDidLoad];    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    self.window.backgroundColor = [NSColor whiteColor];
+    [super windowDidLoad];
 }
 
 - (IBAction)buyNowAction:(id)sender {
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://www.pdfletterhead.net/?utm_source=splashscreen&utm_medium=app&utm_campaign=letterhead-app"]];
 }
-
 
 @end
